92f72c3179f63f121226277700cd2427de5073d2
gcerv93 <gcerv93@protonmail.com>
2022-01-07 17:34:51 -0600

    add player guess display

--- mastermind.rb
+++ mastermind.rb
@@ -39,17 +39,23 @@ module Display
     puts "\nThere are 6 different colors, each represented by a a different number, like so:  \n\n"
     format([1, 2, 3, 4, 5, 6])
     puts "\n\nYou must provide a guess each turn, after which you will be given a hint\n\n"
-    puts "#{circle_hint} - means correct color, wrong position\n"
-    puts "#{filled_circle_hint} - means correct color, correct position\n\n"
+    puts "#{circle} - means correct color, wrong position\n"
+    puts "#{filled_circle} - means correct color, correct position\n\n"
     puts 'You will have 12 turns to figure out the code, Good Luck!'
   end
 
-  def circle_hint
+  def circle
     "\u25CB".encode('utf-8')
   end
 
-  def filled_circle_hint
+  def filled_circle
     "\u25CF".encode('utf-8')
+  end
+
+  def format_hints(hints)
+    hint_string = ''
+    hints.sort.each { |s| hint_string += " #{s}" }
+    hint_string
   end
 end
 
@@ -75,7 +81,6 @@ class Game
     format(code)
   end
 
-  # check if the code given is correct
   def player_guess
     guess = gets.chomp
     if /[1-6]{4}/.match?(guess) && guess.length == 4
@@ -84,5 +89,23 @@ class Game
       puts 'Invalid input! Please only 4 characters of numbers 1-6'
       player_guess
     end
+  end
+
+  def check_guess(guess)
+    hints = []
+    guess.each_with_index do |n, idx|
+      if n == code[idx]
+        hints << filled_circle
+      elsif code.include?(n)
+        hints << circle
+      end
+    end
+    format(guess)
+    display_hints(hints)
+  end
+
+  def display_hints(hints)
+    print "    Hints: #{format_hints(hints)}\n"
+    puts "\nYou cracked the code!" if hints.all? { |h| h == filled_circle }
   end
 end
